fd58a9ab38503ffaaed6268ec466392c8b36a4ed
revanthraj28 <revanthraj135@gmail.com>
2024-05-26 18:02:53 +0530

    removed loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { NavLink, Route,BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
-const Home = lazy(() => import('./pages/Home'));
-const About = lazy(() => import('./pages/About'));
-const Projects = lazy(() => import('./pages/Projects'));
-const Contact = lazy(() => import('./pages/Contact'));
+import { Home, About, Projects, Contact } from './pages';
 
 const App = () => {
   return (
     <main className='bg-slate-300/20 h-full'>
       <Router>
-        <Navbar />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Suspense>
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+        </Routes>
       </Router>
     </main>
-  );
-};
+  )
+}
 
 export default App;
